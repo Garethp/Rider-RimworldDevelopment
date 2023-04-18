c79c137e6d3ad3dc4541da74802b8e88cc08ba41
Gareth <blackfireweb@gmail.com>
2023-04-18 14:22:19 +0100

    Start testing how to reference an assembly that's not part of the Solution

--- src/dotnet/ReSharperPlugin.RimworldDev/ScopeHelper.cs
+++ src/dotnet/ReSharperPlugin.RimworldDev/ScopeHelper.cs
@@ -1,8 +1,15 @@
 using System.Collections.Generic;
 using System.Linq;
+using JetBrains.Application.Threading;
+using JetBrains.Metadata.Reader.API;
 using JetBrains.ProjectModel;
+using JetBrains.ProjectModel.Assemblies.Impl;
+using JetBrains.ProjectModel.model2.Assemblies.Interfaces;
+using JetBrains.ProjectModel.Model2.Assemblies.Interfaces;
 using JetBrains.ReSharper.Psi.Caches;
 using JetBrains.ReSharper.Psi.Modules;
+using JetBrains.ReSharper.TestFramework;
+using JetBrains.Util;
 
 namespace ReSharperPlugin.RimworldDev;
 
@@ -12,6 +19,7 @@ public class ScopeHelper
     private static ISymbolScope rimworldScope;
     private static IPsiModule rimworldModule;
     private static List<ISymbolScope> usedScopes;
+    private static bool adding = false;
 
     public static bool UpdateScopes(ISolution solution)
     {
@@ -24,7 +32,28 @@ public class ScopeHelper
         {
             rimworldScope = allScopes.FirstOrDefault(scope => scope.GetTypeElementByCLRName("Verse.ThingDef") != null);
 
-            if (rimworldScope == null) return false;
+            if (rimworldScope == null)
+            {
+                if (!adding)
+                {
+                    var location =
+                        "C:\\Program Files (x86)\\Steam\\steamapps\\common\\RimWorld\\RimWorldWin64_Data\\Managed\\Assembly-CSharp.dll";
+                    var path = FileSystemPath.TryParse(location);
+
+                    var moduleReferenceResolveContext =
+                        (IModuleReferenceResolveContext)UniversalModuleReferenceContext.Instance;
+
+                    IShellLocks shellLocks = solution.GetComponent<IShellLocks>();
+
+                    // using (shellLocks.UsingWriteLock("Src\\TestFramework\\BaseTestWithSolution.cs"))
+                        solution.GetComponent<IAssemblyFactory>().AddRef(path.ToAssemblyLocation(),
+                            "SolutionTestExtensions::AddAssembly", moduleReferenceResolveContext);
+
+                    adding = true;
+                }
+
+                return false;
+            };
             
             rimworldModule = solution.PsiModules().GetModules()
                 .First(module => module.GetPsiServices().Symbols.GetSymbolScope(module, true, true).GetTypeElementByCLRName("Verse.ThingDef") != null);
