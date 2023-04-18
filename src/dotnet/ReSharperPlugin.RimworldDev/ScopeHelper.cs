1a2ae0b029a6258be178db1aca25503c279abf2e
Gareth Parker <blackfireweb@gmail.com>
2023-04-18 16:02:24 +0100

    Fallback to the on-disk DLL as Reference if the DLL isn't referenced in the solution (#5)
    Start enabling automatic detection of the Rimworld DLL when the Solution doesn't reference one

--- src/dotnet/ReSharperPlugin.RimworldDev/ScopeHelper.cs
+++ src/dotnet/ReSharperPlugin.RimworldDev/ScopeHelper.cs
@@ -1,8 +1,13 @@
 using System.Collections.Generic;
 using System.Linq;
+using JetBrains.Application.Threading.Tasks;
+using JetBrains.Metadata.Reader.API;
 using JetBrains.ProjectModel;
+using JetBrains.ProjectModel.model2.Assemblies.Interfaces;
 using JetBrains.ReSharper.Psi.Caches;
 using JetBrains.ReSharper.Psi.Modules;
+using JetBrains.Util;
+using JetBrains.Util.Threading.Tasks;
 
 namespace ReSharperPlugin.RimworldDev;
 
@@ -12,11 +17,12 @@ public class ScopeHelper
     private static ISymbolScope rimworldScope;
     private static IPsiModule rimworldModule;
     private static List<ISymbolScope> usedScopes;
+    private static bool adding = false;
 
     public static bool UpdateScopes(ISolution solution)
     {
         if (solution == null) return false;
-        
+
         allScopes = solution.PsiModules().GetModules().Select(module =>
             module.GetPsiServices().Symbols.GetSymbolScope(module, true, true)).ToList();
 
@@ -24,15 +30,48 @@ public class ScopeHelper
         {
             rimworldScope = allScopes.FirstOrDefault(scope => scope.GetTypeElementByCLRName("Verse.ThingDef") != null);
 
-            if (rimworldScope == null) return false;
+            if (rimworldScope == null)
+            {
+                AddRef(solution);
+
+                return false;
+            }
             
             rimworldModule = solution.PsiModules().GetModules()
-                .First(module => module.GetPsiServices().Symbols.GetSymbolScope(module, true, true).GetTypeElementByCLRName("Verse.ThingDef") != null);
-
-            // rimworldScope = rimWorldModule.GetPsiServices().Symbols.GetSymbolScope(rimWorldModule, true, true);
+                .First(module =>
+                    module.GetPsiServices().Symbols.GetSymbolScope(module, true, true)
+                        .GetTypeElementByCLRName("Verse.ThingDef") != null);
         }
 
         return true;
+    }
+
+    private static async void AddRef(ISolution solution)
+    {
+        if (adding) return;
+        adding = true;
+
+        var locations = new List<string>
+        {
+            "C:\\Program Files (x86)\\Steam\\steamapps\\common\\RimWorld\\RimWorldWin64_Data\\Managed\\Assembly-CSharp.dll",
+            "C:\\Program Files\\Steam\\steamapps\\common\\RimWorld\\RimWorldWin64_Data\\Managed\\Assembly-CSharp.dll",
+            "~/.steam/steam/SteamApps/common/RimWorld/RimWorldWin64_Data/Managed/Assembly-CSharp.dll"
+        };
+
+
+        var location = locations.FirstOrDefault(location => FileSystemPath.TryParse(location).ExistsFile);
+
+        if (location == null) return;
+        
+        var path = FileSystemPath.TryParse(location);
+
+        var moduleReferenceResolveContext =
+            (IModuleReferenceResolveContext)UniversalModuleReferenceContext.Instance;
+
+        await solution.Locks.Tasks.YieldTo(solution.GetLifetime(), Scheduling.MainDispatcher, TaskPriority.Low);
+
+        solution.GetComponent<IAssemblyFactory>().AddRef(path.ToAssemblyLocation(), "ScopeHelper::AddRef",
+            moduleReferenceResolveContext);
     }
 
     public static ISymbolScope RimworldScope => rimworldScope;
