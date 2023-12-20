692348cb2fb3d3c72638ab95ca31f6fc6eedfab1
Gareth <blackfireweb@gmail.com>
2023-12-20 14:47:17 +0000

    Remove un-used code

--- src/dotnet/ReSharperPlugin.RimworldDev/Navigation/CustomSearcher.cs
+++ src/dotnet/ReSharperPlugin.RimworldDev/Navigation/CustomSearcher.cs
@@ -12,8 +12,8 @@ using JetBrains.ReSharper.Psi.Tree;
 
 namespace ReSharperPlugin.RimworldDev.Navigation;
 
-  public class CustomSearcher<TLanguage> : IDomainSpecificSearcher where TLanguage : KnownLanguage
-  {
+public class CustomSearcher<TLanguage> : IDomainSpecificSearcher where TLanguage : KnownLanguage
+{
     private readonly JetHashSet<string> myNames;
     private readonly JetHashSet<string> myWordsInText;
     private readonly IDeclaredElementsSet myElements;
@@ -22,57 +22,61 @@ namespace ReSharperPlugin.RimworldDev.Navigation;
     private readonly IWordIndex myWordIndex;
 
     public CustomSearcher(
-      IDomainSpecificSearcherFactory factory,
-      IDeclaredElementsSet elements,
-      ReferenceSearcherParameters referenceSearcherParameters,
-      bool searchForLateBound)
+        IDomainSpecificSearcherFactory factory,
+        IDeclaredElementsSet elements,
+        ReferenceSearcherParameters referenceSearcherParameters,
+        bool searchForLateBound)
     {
-      mySearchForLateBound = searchForLateBound;
-      myElements = elements;
-      myReferenceSearcherParameters = referenceSearcherParameters;
-      myNames = new JetHashSet<string>();
-      myWordsInText = new JetHashSet<string>();
-      foreach (IDeclaredElement element in myElements)
-      {
-        myNames.Add(element.ShortName.Split('/').Last());
-        myWordsInText.UnionWith(factory.GetAllPossibleWordsInFile(element));
-      }
-      myWordIndex = myElements.First().GetPsiServices().WordIndex;
+        mySearchForLateBound = searchForLateBound;
+        myElements = elements;
+        myReferenceSearcherParameters = referenceSearcherParameters;
+        myNames = [];
+        myWordsInText = [];
+
+        // The element shortNames are {defType}/{defName} to make them unique references, however in the text they're
+        // just {defName} so we split up their shortName for seraching
+        foreach (var element in myElements)
+        {
+            myNames.Add(element.ShortName.Split('/').Last());
+            myWordsInText.UnionWith(factory.GetAllPossibleWordsInFile(element));
+        }
+
+        myWordIndex = myElements.First().GetPsiServices().WordIndex;
     }
 
     public bool ProcessProjectItem<TResult>(
-      IPsiSourceFile sourceFile,
-      IFindResultConsumer<TResult> consumer)
+        IPsiSourceFile sourceFile,
+        IFindResultConsumer<TResult> consumer)
     {
-      if (CanContainWord(sourceFile))
-      {
+        if (!CanContainWord(sourceFile)) return false;
+
         foreach (ITreeNode psiFile in sourceFile.GetPsiFiles<TLanguage>())
         {
-          if (ProcessElement(psiFile, consumer))
-            return true;
+            if (ProcessElement(psiFile, consumer))
+                return true;
         }
-      }
-      return false;
+
+        return false;
     }
 
     private bool CanContainWord([NotNull] IPsiSourceFile sourceFile)
     {
-      foreach (string word in myWordsInText)
-      {
-        if (myWordIndex.CanContainWord(sourceFile, word))
-          return true;
-      }
-      return false;
+        return myWordsInText.Any(word => myWordIndex.CanContainWord(sourceFile, word));
     }
 
     public bool ProcessElement<TResult>(ITreeNode element, IFindResultConsumer<TResult> consumer)
     {
-      Assertion.Assert(element != null);
-      JetHashSet<string> referenceNames = new JetHashSet<string>();
-      foreach (IDeclaredElement e2 in myElements)
-      {
-        myNames.Add(e2.ShortName);
-      }
-      return (!mySearchForLateBound ? new ReferenceSearchSourceFileProcessor<TResult>(element, myReferenceSearcherParameters, consumer, myElements, myWordsInText, referenceNames) : (NamedThingsSearchSourceFileProcessor) new LateBoundReferenceSourceFileProcessor<TResult>(element, consumer, myElements, myWordsInText, referenceNames)).Run() == FindExecution.Stop;
+        Assertion.Assert(element != null);
+        JetHashSet<string> referenceNames = [];
+        foreach (var myElement in myElements)
+        {
+            myNames.Add(myElement.ShortName);
+        }
+
+        return (!mySearchForLateBound
+            ? new ReferenceSearchSourceFileProcessor<TResult>(element, myReferenceSearcherParameters, consumer,
+                myElements, myWordsInText, referenceNames)
+            : (NamedThingsSearchSourceFileProcessor)new LateBoundReferenceSourceFileProcessor<TResult>(element,
+                consumer, myElements, myWordsInText, referenceNames)).Run() == FindExecution.Stop;
     }
-  }
+}
