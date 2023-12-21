65d691325174d4b5a0a526ff01b04b46c90e7163
Gareth <blackfireweb@gmail.com>
2023-12-21 13:57:01 +0000

    Cutting down the DefGenerator to simplify it

--- src/dotnet/ReSharperPlugin.RimworldDev/Generator/DefGenerator.cs
+++ src/dotnet/ReSharperPlugin.RimworldDev/Generator/DefGenerator.cs
@@ -1,13 +1,12 @@
 using System.Collections.Generic;
 using JetBrains.Application.DataContext;
-using JetBrains.ReSharper.Feature.Services.CSharp.Generate;
 using JetBrains.ReSharper.Feature.Services.Generate;
 using JetBrains.ReSharper.Feature.Services.Generate.Actions;
 using JetBrains.ReSharper.Feature.Services.Generate.Workflows;
 using JetBrains.ReSharper.Psi.CSharp;
 using JetBrains.ReSharper.Psi.Tree;
+using JetBrains.ReSharper.Psi.Xml;
 using JetBrains.ReSharper.Psi.Xml.Resources;
-using JetBrains.Util;
 
 namespace ReSharperPlugin.RimworldDev.Generator;
 
@@ -22,9 +21,10 @@ public class DefGeneratorItemProvider : IGenerateWorkflowProvider
 
 public class GenerateDefWorkflow : GenerateCodeWorkflowBase
 {
-    // Using named parameters for clarification
     public GenerateDefWorkflow()
-        : base(kind: "DefGenerator", icon: PsiXmlThemedIcons.XmlNode.Id, title: "&GenerateDef",
+        : base(kind: "DefGenerator",
+            icon: PsiXmlThemedIcons.XmlNode.Id,
+            title: "&GenerateDef",
             actionGroup: GenerateActionGroup.CLR_LANGUAGE,
             windowTitle: "Generate def",
             description: "Generate a Def()",
@@ -32,10 +32,7 @@ public class GenerateDefWorkflow : GenerateCodeWorkflowBase
     {
     }
 
-    public override double Order
-    {
-        get { return 100; }
-    }
+    public override double Order => 100;
 
     public override bool IsAvailable(IDataContext dataContext)
     {
@@ -53,52 +50,24 @@ public class GenerateDefWorkflow : GenerateCodeWorkflowBase
     }
 }
 
-[GeneratorBuilder("DefGenerator", typeof(CSharpLanguage))]
-public class DefGeneratorBuilder : GeneratorBuilderBase<CSharpGeneratorContext>
+[GeneratorBuilder("DefGenerator", typeof(XmlLanguage))]
+public class DefGeneratorBuilder : GeneratorBuilderBase<GeneratorContextBase>
 {
-    protected override bool IsAvailable(CSharpGeneratorContext context)
+    protected override bool IsAvailable(GeneratorContextBase context)
     {
         return true;
     }
-    
+
     protected override void BuildOptions(
-        CSharpGeneratorContext context,
+        GeneratorContextBase context,
         ICollection<IGeneratorOption> options)
     {
-        var scope = ScopeHelper.RimworldScope;
-        if (scope is null) return;
-        var def = scope.GetTypeElementByCLRName("Verse.Def");
-
-        
-        // if ((context.ClassDeclaration.DeclaredElement as IClass).IsStaticClass() || !(context.Kind == "Constructor"))
-            // return;
-
-            var defOption = new GeneratorOptionSelector("Properties", "Properties",
-                new List<string>() { "defName", "description" });
-
-            defOption.HasDependentOptions = true;
-            
-        options.Add(defOption);
-        context.ProvidedElements.Add(new GeneratorDeclaredElement(def.Fields.FirstNotNull()));
-        // options.Add((IGeneratorOption) new GeneratorOptionSelector("AccessRights", JetBrains.ReSharper.Feature.Services.CSharp.Resources.Strings.GenerateOption_AccessRights_Text, CSharpBuilderOptions.GetAccessRightsChoicesWithAutomatic(context)));
+        options.Add(new GeneratorOptionSelector("Properties", "Properties", new List<string>() { "defName", "description" }));
     }
 
-    protected override bool HasProcessableElements(CSharpGeneratorContext context, IEnumerable<IGeneratorElement> elements)
+    protected override bool HasProcessableElements(GeneratorContextBase context,
+        IEnumerable<IGeneratorElement> elements)
     {
         return true;
     }
 }
-
-// [GeneratorElementProvider("DefGenerator", typeof(CSharpLanguage))]
-// public class DefGeneratorElementProvider : GeneratorProviderBase<CSharpGeneratorContext>
-// {
-//     public override void Populate(CSharpGeneratorContext context)
-//     {
-//         var scope = ScopeHelper.RimworldScope;
-//         if (scope is null) return;
-//
-//         var def = scope.GetTypeElementByCLRName("Verse.Def");
-//         var a = 1 + 1;
-//         context.ProvidedElements.Add(new GeneratorDeclaredElement(def.Fields.FirstNotNull()));
-//     }
-// }
