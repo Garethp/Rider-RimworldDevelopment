8f89003cde44d5b9e332fe0893ca6f8673cb104c
Gareth Parker <blackfireweb@gmail.com>
2024-03-24 14:33:06 +0000

    Adding Modlist and Autosave loading into the RunConfig for Rimworld Config (#40)
    Adding Modlist and Autosave loading into the RunConfig for Rimworld Config (#40)

---
+++ src/rider/main/kotlin/run/QuickStartUtils.kt
@@ -0,0 +1,55 @@
+package RimworldDev.Rider.run
+
+import com.intellij.util.io.delete
+import kotlin.io.path.*
+
+class QuickStartUtils {
+    companion object {
+        fun setup(modListPath: String, saveFilePath: String) {
+            val location = Path(System.getenv("APPDATA"), "..", "LocalLow", "Ludeon Studios", "RimWorld by Ludeon Studios")
+            val configLocation = Path(location.absolutePathString(), "Config")
+            val autostartFile = Path(location.absolutePathString(), "Saves", "autostart.rws")
+            val saveFile = Path(saveFilePath)
+            val modListFile = Path(modListPath)
+
+            if (!modListFile.isRegularFile()) return;
+            if (!configLocation.isDirectory()) return;
+
+            if (saveFile.isRegularFile()) {
+                if (autostartFile.isRegularFile()) {
+                    autostartFile.copyTo(Path(location.absolutePathString(), "Saves", "autostart.rws.rider-bak"), true)
+                }
+
+                saveFile.copyTo(autostartFile, true)
+            }
+
+            val existingFile = Path(configLocation.absolutePathString(), "ModsConfig.xml").toFile()
+            existingFile.copyTo(Path(configLocation.absolutePathString(), "ModsConfig.xml.rider-bak").toFile(), true)
+
+            modListFile.toFile().copyTo(existingFile, true)
+        }
+
+        fun tearDown(saveFilePath: String) {
+            Thread.sleep(50)
+
+            val location = Path(System.getenv("APPDATA"), "..", "LocalLow", "Ludeon Studios", "RimWorld by Ludeon Studios")
+            val configLocation = Path(location.absolutePathString(), "Config")
+            val backupFile = Path(configLocation.absolutePathString(), "ModsConfig.xml.rider-bak")
+            val autostartFile = Path(location.absolutePathString(), "Saves", "autostart.rws")
+            val autostartBackupFile = Path(location.absolutePathString(), "Saves", "autostart.rws.rider-bak")
+
+            if (backupFile.isRegularFile() && backupFile.exists()) {
+                backupFile.copyTo(Path(configLocation.absolutePathString(), "ModsConfig.xml"), true)
+                backupFile.delete(false)
+            }
+
+            if (Path(saveFilePath).exists() && autostartFile.isRegularFile() && autostartFile.exists()) {
+                autostartFile.deleteExisting()
+            }
+
+            if (autostartBackupFile.isRegularFile() && autostartBackupFile.exists()) {
+                autostartBackupFile.copyTo(autostartFile)
+            }
+        }
+    }
+}
