0ba51fb311b5bbce65bc37a1e73104d9cd89a3e8
caleb <caleb@becomingmachinic.com>
2025-07-06 18:37:27 -0500

    Simplify buildPlugin.sh

--- buildPlugin.sh
+++ buildPlugin.sh
@@ -4,10 +4,4 @@ set -e
 
 export Version="2024.1-EAP"
 
-cd "src/dotnet/ReSharperPlugin.RimworldDev"
-FrameworkPathOverride=$(dirname $(which dotnet))/../../../../lib/dotnet/sdk/7.0.202/ dotnet build  ReSharperPlugin.RimworldDev.csproj /property:Configuration=Release
-FrameworkPathOverride=$(dirname $(which dotnet))/../../../../lib/dotnet/sdk/7.0.202/ dotnet build  ReSharperPlugin.RimworldDev.Rider.csproj /property:Configuration=Release
-
-cd -
-
-./gradlew :buildPlugin -x compileDotNet
+./gradlew :buildPlugin
